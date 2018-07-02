27f7e0f5df8cc4897cb614c55529732a25c56aeb
Sean Oughton <seanoughton@gmail.com>
2018-07-02 20:04:42 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,9 @@
 function paragraphSelector() {
   return $('p')
 };
+
+//Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
+
+function lastImageSelector() {
+
+};
