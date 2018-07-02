eea08be199bbfdd0d7e64947854c6e129d8a1f5f
Sean Oughton <seanoughton@gmail.com>
2018-07-02 20:07:02 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,7 +8,6 @@ function lastImageSelector() {
   return $('img:last')
 };
 
-//Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
 function ninjaBabySelector() {
   return $('#baby-ninja')
 };
