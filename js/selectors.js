51412eb909668dbcf8d64cb556ed64ec1c638265
Sean Oughton <seanoughton@gmail.com>
2018-07-02 20:03:58 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,5 +3,5 @@
 //Write a function paragraphSelector that does not accept any parameters. The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
 
 function paragraphSelector() {
-
+  return $('p')
 };
